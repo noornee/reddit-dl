a8f63f096fde27dd3b57f0e1c12ff2f7e9786b99
noornee <noornee.011@gmail.com>
2022-12-30 10:22:21 +0100

    feat: integrated urfave/cli

--- cmd/reddit-dl/main.go
+++ cmd/reddit-dl/main.go
@@ -1,41 +1,53 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"os"
-	"strings"
 
 	"github.com/noornee/reddit-dl/handler"
 	"github.com/noornee/reddit-dl/service"
 	"github.com/noornee/reddit-dl/utility"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+
+	// default url incase the url flag isnt passed
 	var raw_url string
 
-	flag.StringVar(&raw_url, "url", "", "url to parse")
-	flag.Parse()
-
-	passed := utility.IsFlagPassed(raw_url)
-
-	// if a flag isnt passed and a url is passed, assign the url to raw_url
-
-	/*
-		perhaps this?
-		if !(len(raw_url) > 0) {
-			...
-		}
-	*/
-	if len(os.Args) > 1 && !strings.HasSuffix(os.Args[1], "-url") {
+	if len(os.Args) > 1 {
 		raw_url = os.Args[1]
-	} else {
-		if !passed {
-			flag.Usage()
-			return
-		}
-
 	}
 
+	app := &cli.App{
+		Name:  "reddit-dl",
+		Usage: "A reddit multimedia downloader",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "url",
+				Usage: "a reddit post url",
+				Value: raw_url,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			url := ctx.String("url")
+			if url == "" {
+				cli.ShowAppHelp(ctx)
+			}
+
+			controller(url)
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println()
+		utility.ErrorLog.Println(err)
+	}
+
+}
+
+func controller(raw_url string) {
 	url, title := handler.ParseUrl(raw_url)
 
 	// body -> the url response body in form of []bytes
@@ -46,15 +58,14 @@ func main() {
 
 	// media -> an array of string(s) containing the url
 	media_url, err := utility.ParseJSONBody(body)
+
 	if err != nil {
 		utility.ErrorLog.Fatal(err)
-		// return Fatal calls Print, then os.Exit(1), can't return.
 	}
-	//fmt.Println(media_url)
 
 	for _, url := range media_url {
 
-		// if it's a reddit gallery kinda image, then it's going to contain multiple urls
+		// if it's a reddit gallery(multiple images in a post), then it's going to contain multiple urls
 		// its length would be greater than 1
 		if len(media_url) <= 1 {
 			media, audio := utility.GetMediaUrl(url)
--- go.mod
+++ go.mod
@@ -2,17 +2,19 @@ module github.com/noornee/reddit-dl
 
 go 1.19
 
-retract (
-    v0.5.1 // Published accidentally.
-)
+retract v0.5.1 // Published accidentally.
 
 require (
 	github.com/cavaliergopher/grab/v3 v3.0.1
 	github.com/u2takey/ffmpeg-go v0.4.1
+	github.com/urfave/cli/v2 v2.23.7
 )
 
 require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
