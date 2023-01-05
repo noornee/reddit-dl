291c7647741e6afe938ef391948ace2e95239754
noornee <noornee.011@gmail.com>
2023-01-05 11:28:03 +0100

    chore: minor refactoring

--- utility/utility.go
+++ utility/utility.go
@@ -30,45 +30,21 @@ func ParseJSONBody(file []byte) ([]string, error) {
 		if data.MediaMetadata != nil {
 			for _, v := range data.MediaMetadata {
 				image_url := v.S.URL
+				// fmt.Println("debug:0")
 				url := strings.ReplaceAll(image_url, "amp;", "")
 				urls = append(urls, url)
 			}
 			return urls, nil
 		}
 
-		// for crossposts media
+		// for crosspost videos
 		if data.CrossPost != nil {
-
-			// for cases where the crosspost media is nil
-			if data.CrossPost[0].SecureMedia == nil {
-				url := data.CrossPost[0].URLOverriddenByDest
+			if data.CrossPost[0].SecureMedia != nil && data.CrossPost[0].SecureMedia.RedditVideo != nil {
+				url := data.CrossPost[0].SecureMedia.RedditVideo.FallbackURL
 				urls = append(urls, url)
+				// fmt.Println("debug:1")
 				return urls, nil
-
 			}
-
-			// checks if its an embeded crosspost video
-			if data.CrossPost[0].SecureMedia.Oembed != nil {
-
-				gfycat := "https://gfycat.com"
-				provider_url := data.CrossPost[0].SecureMedia.Oembed.ProviderURL
-
-				switch provider_url {
-				case gfycat:
-					url := strings.ReplaceAll(data.CrossPost[0].SecureMedia.Oembed.ThumbnailURL, "size_restricted.gif", "mobile.mp4")
-					urls = append(urls, url)
-					fmt.Println(urls)
-					return urls, nil
-				default:
-					return urls, fmt.Errorf("unsupported provider \"%s\"", provider_url)
-				}
-			}
-
-			// normal crosspost video
-			url := data.CrossPost[0].SecureMedia.RedditVideo.FallbackURL
-			urls = append(urls, url)
-			return urls, nil
-
 		}
 
 		// for external providers --> specifically gfycat.com
@@ -77,6 +53,7 @@ func ParseJSONBody(file []byte) ([]string, error) {
 			gfycat := "https://gfycat.com"
 			provider_url := data.SecureMedia.Oembed.ProviderURL
 
+			// fmt.Println("debug:2:gfycat")
 			switch provider_url {
 			case gfycat:
 				url := strings.ReplaceAll(data.SecureMedia.Oembed.ThumbnailURL, "size_restricted.gif", "mobile.mp4")
@@ -91,20 +68,23 @@ func ParseJSONBody(file []byte) ([]string, error) {
 		// MP4 variant of embedded video (likely NFSW, data.Preview.Images[0].Variants.GIF cannot be used here (even though there are two structs, obfuscated and nsfw, both are "obfuscated" (blurred.)))
 		if data.Preview.Video != nil {
 			url := data.Preview.Video.FallbackURL
+			// fmt.Println("debug:3: ", url)
 			urls = append(urls, url)
 			return urls, nil
 		}
 
 		// GIF variant of embedded video
-		if data.Preview.Images[0].Variants.GIF != nil {
-			url := data.Preview.Images[0].Variants.GIF.Source.URL
-			urls = append(urls, url)
-			return urls, nil
-		}
+		// if data.Preview.Images[0].Variants.GIF != nil {
+		// 	url := data.Preview.Images[0].Variants.GIF.Source.URL
+		// 	// fmt.Println("debug:4")
+		// 	urls = append(urls, url)
+		// 	return urls, nil
+		// }
 
 		// if securemedia is nil then it's a normal image/gif
 		if data.SecureMedia.RedditVideo == nil && data.SecureMedia.Oembed == nil {
 			url := data.URLOverriddenByDest
+			// fmt.Println("debug:5")
 			urls = append(urls, url)
 			return urls, nil
 		}
@@ -112,6 +92,7 @@ func ParseJSONBody(file []byte) ([]string, error) {
 		// for normal reddit video
 		if data.SecureMedia.RedditVideo != nil {
 			url := data.SecureMedia.RedditVideo.FallbackURL
+			// fmt.Println("debug:6")
 			urls = append(urls, url)
 			return urls, nil
 		}
