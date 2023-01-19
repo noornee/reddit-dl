3ac15b86901fdea0a9dfd599df4b1f5d0f4a0e59
noornee <noornee.011@gmail.com>
2023-01-19 23:43:50 +0100

    chore(style): color package

---
+++ color/colors.go
@@ -0,0 +1,29 @@
+package color
+
+import "runtime"
+
+var (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+)
+
+func init() {
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Red = ""
+		Green = ""
+		Yellow = ""
+		Blue = ""
+		Purple = ""
+		Cyan = ""
+		Gray = ""
+		White = ""
+	}
+}
